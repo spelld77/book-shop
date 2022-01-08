3a8aac56dfc16369a02ebefb3048d2a5d4da40d0
Jin <spelld77@gmail.com>
2022-01-08 12:10:45 +0900

    first commit

---
+++ src/main/java/com/shop/portshop/PortshopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.portshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/portshop/PortshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.portshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
