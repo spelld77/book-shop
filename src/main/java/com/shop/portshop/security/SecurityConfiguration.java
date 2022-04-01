a477e3e242d03b161cc1c13a020cf2d71c72246c
Jin <spelld77@gmail.com>
2022-04-01 19:47:35 +0900

    enable csrf token

--- src/main/java/com/shop/portshop/security/SecurityConfiguration.java
+++ src/main/java/com/shop/portshop/security/SecurityConfiguration.java
@@ -32,7 +32,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/login", "/join")
                 .permitAll()
